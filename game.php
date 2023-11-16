110ed4cb5296befa5417fe3a9c67305f831b2a3d
Fel1xGarc1a <131287360+Fel1xGarc1a@users.noreply.github.com>
2023-11-16 13:22:23 -0500

    Add files via upload

---
+++ game.php
@@ -0,0 +1,142 @@
+<?php
+session_start();
+
+// Include your questions file
+include 'questions.php';
+
+// Function to display the game board
+function displayBoard($questions) {
+    // Find the maximum number of questions in any category
+    $maxQuestions = 0;
+    foreach ($questions as $category => $items) {
+        $maxQuestions = max($maxQuestions, count($items));
+    }
+
+    echo '<table>';
+
+    // Create the header row with categories
+    echo '<tr>';
+    foreach ($questions as $category => $items) {
+        echo '<th>' . $category . '</th>';
+    }
+    echo '</tr>';
+
+    // Create rows for questions
+    for ($i = 0; $i < $maxQuestions; $i++) {
+        echo '<tr>';
+        foreach ($questions as $category => $items) {
+            if (isset($items[$i])) {
+                $item = $items[$i];
+                if (isset($_SESSION['answered'][$category][$item['value']])) {
+                    echo '<td>---</td>';
+                } else {
+                    echo '<td><a href="?category=' . urlencode($category) . '&value=' . $item['value'] . '">$' . $item['value'] . '</a></td>';
+                }
+            } else {
+                // Empty cell if no question
+                echo '<td></td>';
+            }
+        }
+        echo '</tr>';
+    }
+
+    echo '</table>';
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Jeopardy Game</title>
+</head>
+<body>
+<div class="header">
+        <img src="logo.png" alt="Logo">
+    </div>
+<?php
+// Function to display the question
+function displayQuestion($question, $category, $value) {
+    echo "<div class='question'>";
+    echo "<p>$question</p>";
+    echo '<form action="game.php" method="post">';
+    echo '<input type="hidden" name="category" value="' . $category . '">';
+    echo '<input type="hidden" name="value" value="' . $value . '">';
+    echo '<input type="text" name="answer" required>';
+    echo '<input type="submit" value="Submit Answer">';
+    echo '</form>';
+    echo "</div>";
+}
+
+// Initialize the game
+if (!isset($_SESSION['playerCount']) && isset($_POST['playerCount'])) {
+    $_SESSION['playerCount'] = $_POST['playerCount'];
+    $_SESSION['currentPlayer'] = 1; // Initialize the current player to 1
+    $_SESSION['scores'] = array_fill(1, $_SESSION['playerCount'], 0);
+    $_SESSION['answered'] = array();
+}
+
+// Check if a question was selected
+if (isset($_GET['category']) && isset($_GET['value'])) {
+    $category = $_GET['category'];
+    $value = $_GET['value'];
+
+    // Find and display the question
+    foreach ($questions[$category] as $item) {
+        if ($item['value'] == $value) {
+            displayQuestion($item['question'], $category, $value);
+            break;
+        }
+    }
+} elseif (isset($_POST['category'], $_POST['value'], $_POST['answer'])) {
+    // Handle the answer submission
+$category = $_POST['category'];
+$value = $_POST['value'];
+$playerAnswer = $_POST['answer'];
+
+// Initialize a flag to track whether the answer is correct
+$correctAnswerFound = false;
+
+// Check if the answer is correct
+foreach ($questions[$category] as $item) {
+    if ($item['value'] == $value && strtolower($playerAnswer) == strtolower($item['answer'])) {
+        // Update the score for the current player
+        $_SESSION['scores'][$_SESSION['currentPlayer']] += $value;
+        $correctAnswerFound = true; // Set the flag to true
+        break; // Exit the loop
+    }
+}
+
+// If the correct answer was not found, subtract points
+if (!$correctAnswerFound) {
+    $_SESSION['scores'][$_SESSION['currentPlayer']] -= $value;
+}
+    
+
+    // Switch to the next player
+    $_SESSION['currentPlayer'] = ($_SESSION['currentPlayer'] % $_SESSION['playerCount']) + 1;
+
+    // Mark the question as answered
+    $_SESSION['answered'][$category][$value] = true;
+
+    // Redirect back to the board
+    header("Location: game.php");
+    exit;
+} else {
+    // Display the game board
+    displayBoard($questions);
+}
+//Displaye Players turn
+echo '<div class="PlayerTurn">';
+echo "Player " . $_SESSION['currentPlayer'] . "'s Turn";
+echo '</div>';
+// Display scores
+echo '<div class="scoreboard">';
+for ($i = 1; $i <= $_SESSION['playerCount']; $i++) {
+    echo "<p>Player $i Score: $" . $_SESSION['scores'][$i] . '</p>';
+}
+echo '</div>';
+?>
+</body>
+</html>
---
+++ questions.php
@@ -0,0 +1,41 @@
+<?php
+
+$questions = [
+    "World Capitals" => [
+        ["value" => 100, "question" => "What is the capital of France?", "answer" => "Paris"],
+        ["value" => 200, "question" => "What is the capital of Japan?", "answer" => "Tokyo"],
+        ["value" => 300, "question" => "What is the capital of Australia?", "answer" => "Canberra"],
+        ["value" => 400, "question" => "What is the capital of Canada?", "answer" => "Ottawa"],
+        ["value" => 500, "question" => "What is the capital of Brazil?", "answer" => "Brasilia"],
+    ],
+    "Famous Scientists" => [
+        ["value" => 100, "question" => "Who proposed the theory of relativity?", "answer" => "Albert Einstein"],
+        ["value" => 200, "question" => "Who is known for the laws of motion and universal gravitation?", "answer" => "Isaac Newton"],
+        ["value" => 300, "question" => "Who is the female scientist famous for her work on radioactivity and for discovering radium?", "answer" => "Marie Curie"],
+        ["value" => 400, "question" => "Who is known as the father of modern physics and famous for the law of planetary motion?", "answer" => "Galileo Galilei"],
+        ["value" => 500, "question" => "Who is known for his contributions to the science of evolution?", "answer" => "Charles Darwin"],
+    ],
+    "Oscar-Winning Films" => [
+        ["value" => 100, "question" => "Which film won the Academy Award for Best Picture in 1994 and is famous for the line - Life is like a box of chocolates?", "answer" => "Forrest Gump"],
+        ["value" => 200, "question" => "Which movie, starring Leonardo DiCaprio and Kate Winslet, won 11 Oscars in 1998?", "answer" => "Titanic"],
+        ["value" => 300, "question" => "What is the 2003 fantasy film that won 11 Oscars, including Best Picture?", "answer" => "The Lord of the Rings: The Return of the King"],
+        ["value" => 400, "question" => "Which 2012 film, directed by Ben Affleck, won the Oscar for Best Picture?", "answer" => "Argo"],
+        ["value" => 500, "question" => "Which 2019 film, directed by Bong Joon-ho, became the first non-English language film to win Best Picture?", "answer" => "Parasite"],
+    ],
+    "Great Inventions" => [
+        ["value" => 100, "question" => "Who is credited with inventing the light bulb?", "answer" => "Thomas Edison"],
+        ["value" => 200, "question" => "Who invented the telephone?", "answer" => "Alexander Graham Bell"],
+        ["value" => 300, "question" => "Who is known for inventing the printing press around 1440?", "answer" => "Johannes Gutenberg"],
+        ["value" => 400, "question" => "Who is credited with inventing the World Wide Web in 1989?", "answer" => "Tim Berners-Lee"],
+        ["value" => 500, "question" => "Who invented the polio vaccine?", "answer" => "Jonas Salk"],
+    ],
+    "Famous Landmarks" => [
+        ["value" => 100, "question" => "What is the name of the ancient Incan citadel located in Peru?", "answer" => "Machu Picchu"],
+        ["value" => 200, "question" => "What is the large, ancient statue located in Giza, Egypt?", "answer" => "The Great Sphinx"],
+        ["value" => 300, "question" => "What is the name of the bell located in London, often associated with the clock tower at the north end of the Palace of Westminster?", "answer" => "Big Ben"],
+        ["value" => 400, "question" => "What is the name of the famous tower located in Paris, France?", "answer" => "Eiffel Tower"],
+        ["value" => 500, "question" => "What is the name of the ancient amphitheater located in the center of Rome, Italy?", "answer" => "The Colosseum"],
+    ],
+];
+
+?>
